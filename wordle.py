11a69262f9ac93319cf51d3a0bd68f6dc926d501
Theo Demetriades <theodemetriades@Theos-MacBook-Pro.local>
2022-01-26 10:18:37 -0800

    First commit.

---
+++ wordle.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env/ python3
+
+"""
+wordle.py
+This script finds five letter matches based on known letters in known positions,
+and known letters that aren't in the word.
+"""
+
+__author__ = "Theo Demetriades"
+__version__ = "2022-01-26"
+
+def findMatches(wordFile, letters, nots):
+    wfile = open(wordFile,'r')
+    words = [word.rstrip().lower() for word in wfile]
+
+    matches = []
+    for word in words:
+        match = True
+        for i in range(len(word)):
+            if (letters[i]!=' ' and word[i]!=letters[i]) or word[i] in nots:
+                match = False
+                break
+
+        if match:
+            matches.append(word)
+
+    return matches
+
+
+def main():
+    wordFile = 'wordles.txt'
+    letters = input('Enter known letters (put spaces in places that are unknown): ').lower()
+    nots = input('Enter letters that are not in the word: ').lower()
+    print(findMatches(wordFile, letters, nots))
+
+
+if __name__ == '__main__':
+    main()
