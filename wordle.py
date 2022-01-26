5c7de86adfcf1b4ea8b7e5870eade09d1d3dad00
Theo Demetriades <theodemetriades@Theos-MacBook-Pro.local>
2022-01-26 13:32:15 -0800

    Added functionality for known letters with unknown positions to wordle.py script.

--- wordle.py
+++ wordle.py
@@ -9,17 +9,22 @@ and known letters that aren't in the word.
 __author__ = "Theo Demetriades"
 __version__ = "2022-01-26"
 
-def findMatches(wordFile, letters, nots):
+def findMatches(wordFile, letters, ins, nots):
     wfile = open(wordFile,'r')
     words = [word.rstrip().lower() for word in wfile]
 
     matches = []
     for word in words:
         match = True
-        for i in range(len(word)):
-            if (letters[i]!=' ' and word[i]!=letters[i]) or word[i] in nots:
+        for c in ins:
+            if c not in word:
                 match = False
                 break
+        if match:
+            for i in range(len(word)):
+                if (letters[i]!=' ' and word[i]!=letters[i]) or (word[i] in nots):
+                    match = False
+                    break
 
         if match:
             matches.append(word)
@@ -30,8 +35,9 @@ def findMatches(wordFile, letters, nots):
 def main():
     wordFile = 'wordles.txt'
     letters = input('Enter known letters (put spaces in places that are unknown): ').lower()
+    ins = input('Enter the other letters you know are in the word: ').lower()
     nots = input('Enter letters that are not in the word: ').lower()
-    print(findMatches(wordFile, letters, nots))
+    print(findMatches(wordFile, letters, ins, nots))
 
 
 if __name__ == '__main__':
